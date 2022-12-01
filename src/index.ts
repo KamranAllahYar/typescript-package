c1d83170b1e60b0a2bd64b8149fc4e98cf51839d
Kamran Allah Yar <KamranAllahYar@outlook.com>
2022-12-01 11:52:28 +0500

    second commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import PostEx from './lib/postex';
+import { CreateOrder, CreateOrderResponse } from './lib/types/CreateOrder';
+import { PickupLocationResponse, PickupLocation } from './lib/types/PickupLocation';
+import { OperationalCitiesQuery, OperationalCitiesResponse } from './lib/types/OperationalCity';
+
+export {
+  CreateOrder,
+  CreateOrderResponse,
+  PickupLocationResponse,
+  PickupLocation,
+  OperationalCitiesQuery,
+  OperationalCitiesResponse,
+};
+export default PostEx;
